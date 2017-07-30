0476f5a0ff0ab4f579da868eb32c2e2a604a3a47
khalob <kcognata@emich.edu>
2017-07-30 13:34:32 +0000

    init

---
+++ capture.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+
+import datetime
+import os
+import datetime
+from time import sleep
+from subprocess import call 
+
+call('fswebcam -r 1080x1920 --no-banner /home/khalob/Camera/images/cracking.jpg', shell=True)
+call('git add -A', shell=True)
+call('git commit -m "PI: Taking and uploading photo"', shell=True)
+call('git push origin master', shell=True)
+
