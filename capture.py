a94c0fdd109678c9461a2be8aaf2e2e55de08a9b
khalob <kcognata@emich.edu>
2017-07-30 13:59:02 +0000

    PI: Taking and uploading photo

--- capture.py
+++ capture.py
@@ -7,7 +7,9 @@ import datetime
 from time import sleep
 from subprocess import call 
 
-call('fswebcam -r 1920x1080 --no-banner /home/khalob/Camera/images/cracking.jpg', shell=True)
+
+
+call('fswebcam -r 1920x1080 --no-banner /home/khalob/Camera/images/!date.jpg', shell=True)
 call('git add -A', shell=True)
 call('git commit -m "PI: Taking and uploading photo"', shell=True)
 call('git push origin master', shell=True)
